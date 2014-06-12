aa4d5cd90d316c8e990b75ecf5ae5f61fb6271f8
Gizakor <gizakor@maniacmansion.org>
2014-06-12 20:34:53 +0200

    testing

---
+++ industrial/fabfile.py
@@ -0,0 +1,19 @@
+from fabric.api import local, lcd , prefix , run
+
+def prepare_deployment(branch_name):
+    local('git add -p && git commit')
+    local('git checkout master && git merge ' + branch_name)
+
+
+def deploy():
+    with lcd('/var/django/eve-industrial/industrial/industrial/'):
+        # with prefix('workon env'):
+        # with prefix("/var/django/eve-industrial/industrial/industrial; workon env"):
+        run('pwd')
+        run('cd /var/django/eve-industrial/ && git pull ')
+        #     local('python manage.py migrate industrialweb')
+            #local('python manage.py test myapp')
+
+
+	run("source /var/django/eve-industrial/env/bin/activate && /var/django/eve-industrial/industrial/run.sh")
+    # run("source /var/django/eve-industrial/env/bin/activate && /var/django/eve-industrial/industrial/run.sh")
