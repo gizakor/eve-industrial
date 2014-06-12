a8b9ed5600604619c6c263ac3ac2a4129af1a377
Gizakor <gizakor@maniacmansion.org>
2014-06-12 22:41:37 +0200

    dfgdfgo

--- industrial/gunistart.sh
+++ industrial/gunistart.sh
@@ -20,4 +20,5 @@ export PYTHONPATH=$DJANGODIR:$PYTHONPATH
 # Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
 exec ../env/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
 --workers $NUM_WORKERS \
+--bind=0.0.0.0:9000 \
 --log-level=debug \
