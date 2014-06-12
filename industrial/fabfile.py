a3bf67cb407232d1cb1eaa6af8cac0e05c14fa89
Gizakor <gizakor@maniacmansion.org>
2014-06-12 21:04:26 +0200

    asd

--- industrial/fabfile.py
+++ industrial/fabfile.py
@@ -12,14 +12,14 @@ def deploy():
     	pass
     local('git pull')
     local('git push')
-    with lcd('/var/django/eve-industrial/industrial/industrial/'):
-        # with prefix('workon env'):
-        # with prefix("/var/django/eve-industrial/industrial/industrial; workon env"):
-        run('pwd')
-        run('cd /var/django/eve-industrial/ && git pull ')
+    # with lcd('/var/django/eve-industrial/industrial/industrial/'):
+    #     # with prefix('workon env'):
+    #     # with prefix("/var/django/eve-industrial/industrial/industrial; workon env"):
+    #     run('pwd')
+    run('cd /var/django/eve-industrial/ && git pull ')
         #     local('python manage.py migrate industrialweb')
             #local('python manage.py test myapp')
 
 
-	run("source /var/django/eve-industrial/env/bin/activate && /var/django/eve-industrial/industrial/run.sh")
+    run("source /var/django/eve-industrial/env/bin/activate && /var/django/eve-industrial/industrial/run.sh")
     # run("source /var/django/eve-industrial/env/bin/activate && /var/django/eve-industrial/industrial/run.sh")
