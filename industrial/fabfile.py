45158d1d28719d22e313dd32165db7ed551145f3
Gizakor <gizakor@maniacmansion.org>
2014-06-12 21:49:41 +0200

    s

--- industrial/fabfile.py
+++ industrial/fabfile.py
@@ -20,5 +20,5 @@ def deploy():
         #     local('python manage.py migrate industrialweb')
             #local('python manage.py test myapp')
 
-    with lcd('/var/django/eve-industrial/industrial/'):
-        run("source /var/django/eve-industrial/env/bin/activate && cd /var/django/eve-industrial/industrial &&  ./run.sh")
+    with lcd('/var/django/eve-industrial/industrial'):
+        run("source /var/django/eve-industrial/env/bin/activate &&  ./run.sh")
